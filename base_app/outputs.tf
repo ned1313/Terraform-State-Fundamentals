6eeb63197e6e40fc61440ffdf164014a0395c285
ned1313 <ned.bellavance@gmail.com>
2025-08-19 09:22:11 -0400

    add commands for m2

--- base_app/outputs.tf
+++ base_app/outputs.tf
@@ -1,14 +1,14 @@
 output "ec2_public_dns" {
   description = "The public DNS name of the EC2 web instance"
-  value = aws_instance.web.public_dns
+  value       = aws_instance.web.public_dns
 }
 
 output "public_subnet_ids" {
   description = "The list of IDs for the public subnets created in the VPC"
-  value = module.networking.public_subnets
+  value       = module.networking.public_subnets
 }
 
 output "vpc_id" {
   description = "The ID of the VPC created"
-  value = module.networking.vpc_id
+  value       = module.networking.vpc_id
 }
